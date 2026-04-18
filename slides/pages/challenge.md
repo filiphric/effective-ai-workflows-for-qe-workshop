8453daa73563648904dc6532337fe683bc853b98
Filip Hric <filip@filiphric.sk>
2026-04-18 21:42:29 +0200

    remove slides

--- slides/pages/challenge.md
+++
@@ -1,9 +0,0 @@
----
-layout: center
----
-
-# Practical challenge
-
-<Timer />
-
-<InstallRepo class="absolute top-54 left-11 w-[600px]" />
--- slides/theme/layouts/cover.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="slidev-layout cover">
-    <div class="my-auto w-full">
-      <slot />
-    </div>
-    <Elements />
-  </div>
-</template>
